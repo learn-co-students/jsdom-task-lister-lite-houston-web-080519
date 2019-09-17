88db6ab21f20909dbc2ed0c1746be0147d25fef7
BarretteBanner <barrettebanner@gmail.com>
2019-09-17 17:26:50 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+array = []
+
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let item = document.querySelector('#new-task-description')
+    console.log(item.value)
+    let list = document.querySelector('#tasks')
+    let element = document.createElement('li')
+    list.append(element)
+    element.append( item.value )
+    
+    let button = document.createElement('button')
+    element.append( button )
+    button.innerText = "x"
+   
+    button.addEventListener('click', function(e){
+      element.remove()
+    })
+    
+  })
+
+  
+
+
+
+
 });
+
+
+
+
+
+
+
+
