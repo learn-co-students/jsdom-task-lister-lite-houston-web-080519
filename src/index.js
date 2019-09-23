839ea23626ec49364acc4be482d2e2611f94044f
Jade Huynh <35jadeh@gmail.com>
2019-09-22 23:00:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('create-task-form')
+  let taskInput = document.getElementById('new-task-description')
+
+  form.addEventListener('submit', function(e){
+    
+    e.preventDefault();
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+    li.innerHTML = taskInput.value;
+    ul.append(li);
+    
+  })
+
 });
